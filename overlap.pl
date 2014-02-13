00d3d529d08cdfcb7817dcbf3e5d040db0e8b726
Andrew Quitadamo <aquitada@uncc.edu>
2014-02-13 10:28:13 -0500

    Created extract subroutine
    -Subroutine take the place of #TCGA_hier, TCGA_NMF and marchini

--- overlap.pl
+++ overlap.pl
@@ -263,7 +263,7 @@ sub miRPD_croft()
 
         while ( my ($key, $value) = each(%overlap) )
         {
-                print $key . "=>" . $value . "\n";
+                #print $key . "=>" . $value . "\n";
         }
 }
 
@@ -293,10 +293,45 @@ sub miRPD_miranda()
 
         while ( my ($key, $value) = each(%overlap) )
         {   
-                print $key . "=>" . $value . "\n";
+                #print $key . "=>" . $value . "\n";
         }   
 }
-my @search_files=("mirdsnp-snp-mir-distance.csv#,#4#2#Ovarian","miranda_full.tsv#\t#1#0#DOID:2394","croft_full.tsv#\t#1#0#DOID:2394","Phenomir2.tbl#\t#2#5#Ovarian","miRCancer.txt#\t#1#0#ovarian","miRmasterlist#\t#1#0#ovarian");
+
+my @simple_files=("miRNA-all-hierarchical.txt#\t#0","miRNA-all-NMF.txt#\t#0","miRNA-initial.csv#,#0");
+sub extract
+{
+	shift(@_);
+	my @split = split("#",$_);
+	my $filename = @split[0];
+	my $sep = @split[1];
+	my @miRNA_index = @split[2];
+
+        open(FILE, $filename) || die "Couldn't open file: " . $filename . "\n";
+        my @file = <FILE>;
+        close FILE;
+
+        shift(@file);
+
+        my %miRNA;
+        my @temp;
+        foreach(@file)
+        {
+                @temp = split($sep, $_);
+                $miRNA{lc(@temp[$miRNA_index])}++;
+        }
+
+        while ( my ($key) = each(%miRNA) )
+        {
+                $overlap{$key}++;
+        }
+
+        while ( my ($key, $value) = each(%overlap) )
+        {
+                #print $key . "=>" . $value . "\n";
+        }
+}
+
+my @search_files=("mirdsnp-snp-mir-distance.csv#,#4#2#Ovarian","miranda_full.tsv#\t#1#0#DOID:2394","croft_full.tsv#\t#1#0#DOID:2394","Phenomir2.tbl#\t#2#5#Ovarian","miRCancer.txt#\t#1#0#ovarian","miRmasterlist#\t#1#0#ovarian","HMDD2#\t#2#1#Ovarian");
 sub search
 {
 	shift(@_);
@@ -330,7 +365,7 @@ sub search
        	{
                	$overlap{$key}++;
        	}
-	print $filename . "\n";
+	#print $filename . "\n";
 
 	while ( my ($key, $value) = each(%overlap) )
         {
@@ -343,10 +378,14 @@ foreach(@search_files)
 	search($_);
 }
 
-TCGA_hier;
-TCGA_NMF;
-marchini;
-HMDD2;
+foreach(@simple_files)
+{
+	extract($_);
+}
+#TCGA_hier;
+#TCGA_NMF;
+#marchini;
+#HMDD2;
 #miR2Disease;
 #miRCancer;
 #miRdSNP;
